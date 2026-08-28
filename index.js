0aae015dbd93516c176c24160118d8011656bac1
unknown <pc30@csitlab.local>
2026-08-28 10:01:16 +0800

    feat: server setup

--- index.js
+++ index.js
@@ -4,6 +4,31 @@ const app = require('express')
 
 //Server set up
 const server = app()
-server.listen(1008, '192.168.2.32', () => {
-    console.log('server is running...')
+const PORT = 1007
+const HOSTNAME = '0.0.0.0.'
+
+server.listen(PORT, HOSTNAME, () => {
+console.log('Server is running in ${HOSTNAME}:${PORT}')
 })
+
+let data =[
+{
+    id: 1, 
+    name: 'Dog',
+},
+{
+    id: 2,
+    name: 'Cat',
+},
+{
+    id: 3,
+    name: 'Fish',
+},
+
+
+
+
+
+
+
+]
